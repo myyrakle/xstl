71c33187f3f29a94bf1365b4973eb18c3a55424a
myyrakle <sssang97@naver.com>
2020-02-12 22:43:21 +0900

    add namespace xstl

--- array/fixed_vector.h
+++ array/fixed_vector.h
@@ -1,72 +1,162 @@
 #include <stdexcept>
 #include <utility>
+#include <iterator>
 
-template <class T>
-class fixed_vector
+namespace xstl
 {
-public:
-    using Self = fixed_vector;
-    using value_type = T;
-    using size_type = std::size_t;
-    using difference_type = std::ptrdiff_t;
-    using reference	= value_type&; 
-    using const_reference = const value_type&; 
-    using pointer =	value_type*;
-    using const_pointer	= const value_type*; 
-    class iterator;
-    class const_iterator;
-    using reverse_iterator = std::reverse_iterator<iterator>;
-    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-private:
-    T* array;
-    size_type length;
-public:
-    fixed_vector(): array(nullptr), length(0)
-    {}
+    template <class T>
+    class fixed_vector
+    {
+    public:
+        using Self = fixed_vector;
+        using value_type = T;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using reference	= value_type&; 
+        using const_reference = const value_type&; 
+        using pointer =	value_type*;
+        using const_pointer	= const value_type*; 
+        class iterator;
+        class const_iterator;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    fixed_vector(size_type length): array(new T[length]), length(length)
-    {}
+    private:
+        T* _array;
+        size_type _length;
+    public:
+        fixed_vector(): _array(nullptr), _length(0)
+        {}
 
-    virtual ~fixed_vector()
-    {
-        if(this->array != nullptr)
-            delete[] this->array;
-    }
+        fixed_vector(size_type length): _array(new T[length]), _length(length)
+        {}
 
-public:
-    fixed_vector(const Self&);
-    fixed_vector(Self&&);
-    Self& operator=(const Self&);
-    Self& operator=(Self&&);
+        fixed_vector(size_type length, const_reference value): _array(new T[length]), _length(length)
+        {
+            for(int i=0; i < this->_length; i++)
+                _array[i] = value;
+        }
 
-public:
-    reference operator[](size_type index)
-    {
-        return this->array[index];
-    }
-    const_reference operator[](size_type index) const
-    {
-        return this->array[index];
-    }
-    reference at(size_type index)
-    {
-        if(index >= this->length)
-            throw std::out_of_range();
-        return this->array[index];
-    }
-    const_reference at(size_type index) const
-    {
-        if(index >= this->length)
-            throw std::out_of_range();
-        return this->array[index];
-    }
+        fixed_vector(std::initializer_list<T> init): _array(new T[init.size()]), _length(init.size())
+        {
+            int i = 0;
+            for(auto&&e : init)
+            {
+                _array[i];
+                i++;
+            }
+        }
 
-public:
-    void clear() noexcept
-    {
-        if(this->array != nullptr)
-            delete[] this->array;
-        this->length = 0;
-    }
-};
+        template<class InputIterator>
+        fixed_vector(InputIterator begin, InputIterator end): _length(std::distance(begin, end))
+        {
+            this->_array = new T[this->_length];
+            int i = 0;
+            for(;begin!=end; begin++)
+            {
+                _array[i];
+                i++;
+            }
+        }
+
+        virtual ~fixed_vector()
+        {
+            if(this->_array != nullptr)
+                delete[] this->_array;
+        }
+
+    public: //copy & move
+        fixed_vector(const Self&);
+        fixed_vector(Self&&);
+        Self& operator=(const Self&);
+        Self& operator=(Self&&);
+
+    public:
+        reference operator[](size_type index)
+        {
+            return this->_array[index];
+        }
+        const_reference operator[](size_type index) const
+        {
+            return this->_array[index];
+        }
+        reference at(size_type index)
+        {
+            if(index >= this->_length)
+                throw std::out_of_range();
+            return this->_array[index];
+        }
+        const_reference at(size_type index) const
+        {
+            if(index >= this->_length)
+                throw std::out_of_range();
+            return this->_array[index];
+        }
+
+    public:
+        bool empty() const noexcept
+        {
+            return this->_length == 0;
+        }
+        size_type size() const noexcept
+        {
+            return this->_length;
+        }
+    public:
+        void clear() noexcept
+        {
+            if(this->_array != nullptr)
+            {
+                delete[] this->_array;
+                this->_length = 0;
+            }
+        }
+
+        void assign(size_type length, const_reference value)
+        {
+            this->clear();
+        }
+        void assign(std::initializer_list<T> init)
+        {
+            this->clear();
+        }
+        template <class InputIterator>
+        void assign(InputIterator begin, InputIterator end)
+        {
+            this->clear();
+        }
+
+        void swap(Self& other) noexcept
+        {
+            size_type length_temp = other._length;
+            T* array_temp = other._array;
+
+            other._length = this->_length;
+            other._array = this->_array;
+
+            this->_length = length_temp;
+            this->_array = array_temp;    
+        }
+
+    public: //iterator 
+        iterator begin();
+        const_iterator begin() const noexcept;
+        const_iterator cbegin() const noexcept;
+        
+        iterator end();
+        const_iterator end() const noexcept;
+        const_iterator cend() const noexcept;
+        
+    public: //reserse iterator
+        iterator rbegin();
+        const_iterator rbegin() const noexcept;
+        const_iterator crbegin() const noexcept;
+        
+        iterator rend();
+        const_iterator rend() const noexcept;
+        const_iterator crend() const noexcept;
+        
+    };
+
+}
