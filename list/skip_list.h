bb3a8e0e3bd9c24147d9ff698a6461d02a9170c5
myyrakle <sssang97@naver.com>
2020-02-27 07:39:01 +0900

    add skip_list

---
+++ list/skip_list.h
@@ -0,0 +1,23 @@
+namespace xstl
+{
+    template <class T>
+    class skip_list
+    {
+    public:
+		using Self = skip_list;
+
+	public: //stl standard type member
+		using value_type = T;
+        struct node_type;
+		using size_type = std::size_t;
+		using difference_type = std::ptrdiff_t;
+		using reference = value_type & ;
+		using const_reference = const value_type&;
+		using pointer = value_type *;
+		using const_pointer = const value_type*;
+		class iterator;
+		class const_iterator;
+		using reverse_iterator = std::reverse_iterator<iterator>;
+		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    };
+}
