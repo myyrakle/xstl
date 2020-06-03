b660e95c04e4219df173bd5bfc5d124adac71e69
sanggeon <sssang97@svcorps.dev>
2020-06-04 07:47:15 +0900

    convert tab to space 4

--- array/fixed_vector.h
+++ array/fixed_vector.h
@@ -12,246 +12,246 @@ namespace xstl
 {
 
     template <class T>
-	class fixed_vector
-	{
-	public:
-		using Self = fixed_vector;
+    class fixed_vector
+    {
+    public:
+        using Self = fixed_vector;
 
-	public: //stl standard type member
-		using value_type = T;
-		using size_type = std::size_t;
-		using difference_type = std::ptrdiff_t;
-		using reference = value_type & ;
-		using const_reference = const value_type&;
-		using pointer = value_type * ;
-		using const_pointer = const value_type*;
-		class iterator;
-		class const_iterator;
-		using reverse_iterator = std::reverse_iterator<fixed_vector::iterator>;
-		using const_reverse_iterator = std::reverse_iterator<fixed_vector::const_iterator>;
+    public: //stl standard type member
+        using value_type = T;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using reference = value_type & ;
+        using const_reference = const value_type&;
+        using pointer = value_type * ;
+        using const_pointer = const value_type*;
+        class iterator;
+        class const_iterator;
+        using reverse_iterator = std::reverse_iterator<fixed_vector::iterator>;
+        using const_reverse_iterator = std::reverse_iterator<fixed_vector::const_iterator>;
 
-	private:
-		T* _array = nullptr;
-		size_type _length = 0;
-	public:
-		fixed_vector(size_type length, const_reference value=value_type()) : _array(new value_type[length]), _length(length)
-		{
-			for (int i = 0; i < this->_length; i++)
-				_array[i] = value;
-		}
+    private:
+        T* _array = nullptr;
+        size_type _length = 0;
+    public:
+        fixed_vector(size_type length, const_reference value=value_type()) : _array(new value_type[length]), _length(length)
+        {
+            for (int i = 0; i < this->_length; i++)
+                _array[i] = value;
+        }
 
-		fixed_vector(std::initializer_list<value_type> init) : _array(new value_type[init.size()]), _length(init.size())
-		{
-			int i = 0;
-			for (auto&&e : init)
-			{
-				_array[i] = e;
-				i++;
-			}
-		}
+        fixed_vector(std::initializer_list<value_type> init) : _array(new value_type[init.size()]), _length(init.size())
+        {
+            int i = 0;
+            for (auto&&e : init)
+            {
+                _array[i] = e;
+                i++;
+            }
+        }
 
-		template<class InputIterator>
-		fixed_vector(InputIterator begin, InputIterator end) : _length(std::distance(begin, end))
-		{
-			this->_array = new value_type[this->_length];
-			int i = 0;
-			for (; begin != end; begin++)
-			{
-				_array[i] = *begin;
-				i++;
-			}
-		}
-	public:
-		fixed_vector() = default;
-		~fixed_vector()
-		{
-			if (this->_array != nullptr)
-				delete[] this->_array;
-		}
+        template<class InputIterator>
+        fixed_vector(InputIterator begin, InputIterator end) : _length(std::distance(begin, end))
+        {
+            this->_array = new value_type[this->_length];
+            int i = 0;
+            for (; begin != end; begin++)
+            {
+                _array[i] = *begin;
+                i++;
+            }
+        }
+    public:
+        fixed_vector() = default;
+        ~fixed_vector()
+        {
+            if (this->_array != nullptr)
+                delete[] this->_array;
+        }
 
-	public: //copy & move
-		fixed_vector(const Self& other)
-		{
-			this->_length = other._length;
-			this->_array = new value_type[other._length];
+    public: //copy & move
+        fixed_vector(const Self& other)
+        {
+            this->_length = other._length;
+            this->_array = new value_type[other._length];
 
-			for (int i = 0; i < other._length; i++)
-				this->_array[i] = other._array[i];
-		}
-		fixed_vector(Self&& other)
-		{
-			this->_length = other._length;
-			this->_array = other._array;
-			other._length = 0;
-			other._array = nullptr;
-		}
-		Self& operator=(const Self& other)
-		{
-			this->clear();
+            for (int i = 0; i < other._length; i++)
+                this->_array[i] = other._array[i];
+        }
+        fixed_vector(Self&& other)
+        {
+            this->_length = other._length;
+            this->_array = other._array;
+            other._length = 0;
+            other._array = nullptr;
+        }
+        Self& operator=(const Self& other)
+        {
+            this->clear();
 
-			this->_length = other._length;
-			this->_array = new value_type[other._length];
+            this->_length = other._length;
+            this->_array = new value_type[other._length];
 
-			for (int i = 0; i < other._length; i++)
-				this->_array[i] = other._array[i];
+            for (int i = 0; i < other._length; i++)
+                this->_array[i] = other._array[i];
 
-			return *this;
-		}
-		Self& operator=(Self&& other) 
-		{
-			this->_length = other._length;
-			this->_array = other._array;
-			other._length = 0;
-			other._array = nullptr;
+            return *this;
+        }
+        Self& operator=(Self&& other) 
+        {
+            this->_length = other._length;
+            this->_array = other._array;
+            other._length = 0;
+            other._array = nullptr;
 
-			return *this;
-		}
+            return *this;
+        }
 
-	public:
-		reference operator[](size_type index)
-		{
-			return this->_array[index];
-		}
-		const_reference operator[](size_type index) const
-		{
-			return this->_array[index];
-		}
-		reference at(size_type index)
-		{
-			if (index >= this->_length)
-				throw std::out_of_range();
-			return this->_array[index];
-		}
-		const_reference at(size_type index) const
-		{
-			if (index >= this->_length)
-				throw std::out_of_range();
-			return this->_array[index];
-		}
-		pointer data() noexcept
-		{
-			return this->_array;
-		}
-		const_pointer data() const noexcept
-		{
-			return this->_array;
-		}
+    public:
+        reference operator[](size_type index)
+        {
+            return this->_array[index];
+        }
+        const_reference operator[](size_type index) const
+        {
+            return this->_array[index];
+        }
+        reference at(size_type index)
+        {
+            if (index >= this->_length)
+                throw std::out_of_range();
+            return this->_array[index];
+        }
+        const_reference at(size_type index) const
+        {
+            if (index >= this->_length)
+                throw std::out_of_range();
+            return this->_array[index];
+        }
+        pointer data() noexcept
+        {
+            return this->_array;
+        }
+        const_pointer data() const noexcept
+        {
+            return this->_array;
+        }
 
-	public:
-		bool empty() const noexcept
-		{
-			return this->_length == 0;
-		}
-		size_type size() const noexcept
-		{
-			return this->_length;
-		}
-	public:
-		void clear() noexcept
-		{
-			if (this->_array != nullptr)
-			{
-				delete[] this->_array;
-				this->_length = 0;
-			}
-		}
+    public:
+        bool empty() const noexcept
+        {
+            return this->_length == 0;
+        }
+        size_type size() const noexcept
+        {
+            return this->_length;
+        }
+    public:
+        void clear() noexcept
+        {
+            if (this->_array != nullptr)
+            {
+                delete[] this->_array;
+                this->_length = 0;
+            }
+        }
 
-		void assign(size_type length, const_reference value = value_type())
-		{
-			this->clear();
-			this->_length = length;
-			this->_array = new value_type[length];
-			for (int i = 0; i < this->_length; i++)
-				_array[i] = value;
-		}
-		void assign(std::initializer_list<value_type> init)
-		{
-			this->assign(init.size());
-			int i = 0;
-			for (auto&&e : init)
-			{
-				_array[i] = e;
-				i++;
-			}
-		}
-		template <class InputIterator>
-		void assign(InputIterator begin, InputIterator end)
-		{
-			this->assign(std::distance(begin, end));
-			this->_array = new value_type[this->_length];
-			int i = 0;
-			for (; begin != end; begin++)
-			{
-				_array[i] = *begin;
-				i++;
-			}
-		}
+        void assign(size_type length, const_reference value = value_type())
+        {
+            this->clear();
+            this->_length = length;
+            this->_array = new value_type[length];
+            for (int i = 0; i < this->_length; i++)
+                _array[i] = value;
+        }
+        void assign(std::initializer_list<value_type> init)
+        {
+            this->assign(init.size());
+            int i = 0;
+            for (auto&&e : init)
+            {
+                _array[i] = e;
+                i++;
+            }
+        }
+        template <class InputIterator>
+        void assign(InputIterator begin, InputIterator end)
+        {
+            this->assign(std::distance(begin, end));
+            this->_array = new value_type[this->_length];
+            int i = 0;
+            for (; begin != end; begin++)
+            {
+                _array[i] = *begin;
+                i++;
+            }
+        }
 
-		void swap(Self& other) noexcept
-		{
-			size_type length_temp = other._length;
-			T* array_temp = other._array;
+        void swap(Self& other) noexcept
+        {
+            size_type length_temp = other._length;
+            T* array_temp = other._array;
 
-			other._length = this->_length;
-			other._array = this->_array;
+            other._length = this->_length;
+            other._array = this->_array;
 
-			this->_length = length_temp;
-			this->_array = array_temp;
-		}
+            this->_length = length_temp;
+            this->_array = array_temp;
+        }
 
-	public: //iterator 
-		iterator begin()
-		{
-			return fixed_vector::iterator(this->_array);
-		}
-		fixed_vector::const_iterator begin() const noexcept
-		{
-			return this->cbegin();
-		}
-		fixed_vector::const_iterator cbegin() const noexcept
-		{
-			return fixed_vector::const_iterator(this->_array);
-		}
+    public: //iterator 
+        iterator begin()
+        {
+            return fixed_vector::iterator(this->_array);
+        }
+        fixed_vector::const_iterator begin() const noexcept
+        {
+            return this->cbegin();
+        }
+        fixed_vector::const_iterator cbegin() const noexcept
+        {
+            return fixed_vector::const_iterator(this->_array);
+        }
 
-		fixed_vector::iterator end()
-		{
-			return fixed_vector::iterator(this->_array + this->_length);
-		}
-		fixed_vector::const_iterator end() const noexcept
-		{
-			return this->cend();
-		}
-		fixed_vector::const_iterator cend() const noexcept
-		{
-			return fixed_vector::const_iterator(this->_array + this->_length);
-		}
+        fixed_vector::iterator end()
+        {
+            return fixed_vector::iterator(this->_array + this->_length);
+        }
+        fixed_vector::const_iterator end() const noexcept
+        {
+            return this->cend();
+        }
+        fixed_vector::const_iterator cend() const noexcept
+        {
+            return fixed_vector::const_iterator(this->_array + this->_length);
+        }
 
-	public: //reverse iterator
-		fixed_vector::reverse_iterator rbegin()
-		{
-			return fixed_vector::reverse_iterator(this->end());
-		}
-		fixed_vector::const_reverse_iterator rbegin() const noexcept
-		{
-			return this->crbegin();
-		}
-		fixed_vector::const_reverse_iterator crbegin() const noexcept
-		{
-			return fixed_vector::const_reverse_iterator(this->cend());
-		}
+    public: //reverse iterator
+        fixed_vector::reverse_iterator rbegin()
+        {
+            return fixed_vector::reverse_iterator(this->end());
+        }
+        fixed_vector::const_reverse_iterator rbegin() const noexcept
+        {
+            return this->crbegin();
+        }
+        fixed_vector::const_reverse_iterator crbegin() const noexcept
+        {
+            return fixed_vector::const_reverse_iterator(this->cend());
+        }
         
-		fixed_vector::reverse_iterator rend()
-		{
-			return fixed_vector::reverse_iterator(this->begin());
-		}
-		fixed_vector::const_reverse_iterator rend() const noexcept
-		{
-			return this->crend();
-		}
-		fixed_vector::const_reverse_iterator crend() const noexcept
-		{
-			return fixed_vector::const_reverse_iterator(this->cbegin());
-		}
+        fixed_vector::reverse_iterator rend()
+        {
+            return fixed_vector::reverse_iterator(this->begin());
+        }
+        fixed_vector::const_reverse_iterator rend() const noexcept
+        {
+            return this->crend();
+        }
+        fixed_vector::const_reverse_iterator crend() const noexcept
+        {
+            return fixed_vector::const_reverse_iterator(this->cbegin());
+        }
         
         class iterator
         {
@@ -259,13 +259,13 @@ namespace xstl
             pointer current;
         public:
             using Self = iterator;
-			friend const_iterator;
-		public:
-			using value_type = fixed_vector::value_type;
-			using pointer = fixed_vector::pointer;
-			using reference = fixed_vector::reference;
-			using difference_type = std::ptrdiff_t;
-			using iterator_category = std::random_access_iterator_tag;
+            friend const_iterator;
+        public:
+            using value_type = fixed_vector::value_type;
+            using pointer = fixed_vector::pointer;
+            using reference = fixed_vector::reference;
+            using difference_type = std::ptrdiff_t;
+            using iterator_category = std::random_access_iterator_tag;
         public:
             iterator() = delete;
             virtual ~iterator() = default;
@@ -299,27 +299,27 @@ namespace xstl
                 return *this;
             }
         public: //access operator
-			reference operator*()
+            reference operator*()
             {
                 return *current;
             }
-			const_reference operator*() const
+            const_reference operator*() const
             {
                 return *current;
             }
-			reference operator[](size_type index)
+            reference operator[](size_type index)
             {
                 return current[index];
             }
-			const_reference operator[](size_type index) const
+            const_reference operator[](size_type index) const
             {
                 return current[index];
             }
-			pointer operator->()
+            pointer operator->()
             {
                 return current;
             }
-			const_pointer operator->() const
+            const_pointer operator->() const
             {
                 return current;
             }
@@ -336,72 +336,72 @@ namespace xstl
 
         class const_iterator
         {
-		private:
-			const_pointer current;
-		public:
-			using Self = const_iterator;
-		public:
-			using value_type = value_type;
-			using pointer = pointer;
-			using reference = reference;
-			using difference_type = std::ptrdiff_t;
-			using iterator_category = std::random_access_iterator_tag;
-		public:
-			const_iterator() = delete;
-			virtual ~const_iterator() = default;
-		public: //move & copy member
-			const_iterator(const Self&) = default;
-			const_iterator(Self&&) = default;
-			Self& operator=(const Self&) = default;
-			Self& operator=(Self&&) = default;
-		public:
-			const_iterator(const_pointer p) : current(p)
-			{}
-			const_iterator(iterator it) : current(it.current)
-			{}
-		public: //move operator
-			Self& operator++()
-			{
-				current++;
-				return *this;
-			}
-			Self operator++(int)
-			{
-				current++;
-				return *this;
-			}
-			Self& operator--()
-			{
-				current--;
-				return *this;
-			}
-			Self operator--(int)
-			{
-				current--;
-				return *this;
-			}
-		public: //access operator
-			const_reference operator*() const
-			{
-				return *current;
-			}
-			const_reference operator[](size_type index) const
-			{
-				return current[index];
-			}
-			const_pointer operator->() const
-			{
-				return current;
-			}
-		public: //comparer
-			bool operator==(const Self& other) const
-			{
-				return this->current == other.current;
-			}
-			bool operator!=(const Self& other) const
-			{
-				return this->current != other.current;
-			}
+        private:
+            const_pointer current;
+        public:
+            using Self = const_iterator;
+        public:
+            using value_type = value_type;
+            using pointer = pointer;
+            using reference = reference;
+            using difference_type = std::ptrdiff_t;
+            using iterator_category = std::random_access_iterator_tag;
+        public:
+            const_iterator() = delete;
+            virtual ~const_iterator() = default;
+        public: //move & copy member
+            const_iterator(const Self&) = default;
+            const_iterator(Self&&) = default;
+            Self& operator=(const Self&) = default;
+            Self& operator=(Self&&) = default;
+        public:
+            const_iterator(const_pointer p) : current(p)
+            {}
+            const_iterator(iterator it) : current(it.current)
+            {}
+        public: //move operator
+            Self& operator++()
+            {
+                current++;
+                return *this;
+            }
+            Self operator++(int)
+            {
+                current++;
+                return *this;
+            }
+            Self& operator--()
+            {
+                current--;
+                return *this;
+            }
+            Self operator--(int)
+            {
+                current--;
+                return *this;
+            }
+        public: //access operator
+            const_reference operator*() const
+            {
+                return *current;
+            }
+            const_reference operator[](size_type index) const
+            {
+                return current[index];
+            }
+            const_pointer operator->() const
+            {
+                return current;
+            }
+        public: //comparer
+            bool operator==(const Self& other) const
+            {
+                return this->current == other.current;
+            }
+            bool operator!=(const Self& other) const
+            {
+                return this->current != other.current;
+            }
         };
     };
 
--- heap/heap.h
+++ heap/heap.h
@@ -6,158 +6,158 @@
 
 namespace xstl 
 {
-	template <class T, class Compare = std::less<T>, class Container=std::vector<T> >
-	class heap;
+    template <class T, class Compare = std::less<T>, class Container=std::vector<T> >
+    class heap;
 
-	template <class T>
-	using max_heap = heap<T, std::less<T>>;
-	template <class T>
-	using min_heap = heap<T, std::greater<T>>;
+    template <class T>
+    using max_heap = heap<T, std::less<T>>;
+    template <class T>
+    using min_heap = heap<T, std::greater<T>>;
 
-	template <class T, class Compare, class Container>
-	class heap
-	{
-	public:
-		using Self = heap;
-	public:
-		using container_type = Container;
-		using value_type = typename Container::value_type;
-		using size_type = typename Container::size_type;
-		using refernce = typename Container::reference;
-		using const_reference = typename Container::const_reference;
-		using pointer = typename Container::pointer;
-		using const_pointer = typename Container::const_pointer;
-		
-	private:
-		Container _container;
-	private:
-		void _push_heap()
-		{
-			std::push_heap(_container.begin(), _container.end(), Compare());
-		}
-		void _make_heap()
-		{
-			std::make_heap(_container.begin(), _container.end(), Compare());
-		}
-	
-	public:
-		heap() = default;
-		virtual ~heap() = default;
-	public: //기본 생성/대입자
-		heap(const Self&) = default;
-		heap(Self&&) = default;
-		Self& operator=(const Self&) = default;
-		Self& operator=(Self&&) = default;
+    template <class T, class Compare, class Container>
+    class heap
+    {
+    public:
+        using Self = heap;
+    public:
+        using container_type = Container;
+        using value_type = typename Container::value_type;
+        using size_type = typename Container::size_type;
+        using refernce = typename Container::reference;
+        using const_reference = typename Container::const_reference;
+        using pointer = typename Container::pointer;
+        using const_pointer = typename Container::const_pointer;
+        
+    private:
+        Container _container;
+    private:
+        void _push_heap()
+        {
+            std::push_heap(_container.begin(), _container.end(), Compare());
+        }
+        void _make_heap()
+        {
+            std::make_heap(_container.begin(), _container.end(), Compare());
+        }
+    
+    public:
+        heap() = default;
+        virtual ~heap() = default;
+    public: //기본 생성/대입자
+        heap(const Self&) = default;
+        heap(Self&&) = default;
+        Self& operator=(const Self&) = default;
+        Self& operator=(Self&&) = default;
 
-	public: //constructor
-		heap(std::initializer_list<value_type> init) : _container(init)
-		{
-			this->_make_heap();
-		}
-		template<class InputIterator>
-		heap(InputIterator begin, InputIterator end): _container(begin, end)
-		{
-			this->_make_heap();
-		}
-		heap(size_type count, const_reference value): _container(count, value)
-		{
-			this->_make_heap();
-		}
+    public: //constructor
+        heap(std::initializer_list<value_type> init) : _container(init)
+        {
+            this->_make_heap();
+        }
+        template<class InputIterator>
+        heap(InputIterator begin, InputIterator end): _container(begin, end)
+        {
+            this->_make_heap();
+        }
+        heap(size_type count, const_reference value): _container(count, value)
+        {
+            this->_make_heap();
+        }
 
-		heap& operator=(std::initializer_list<value_type> init)
-		{
-			_container = init;
-			this->_make_heap();
-		}
-	public:
-		void assign(size_type count, const_reference value)
-		{
-			_container.assign(count, value);
-			this->_make_heap();
-		}
-		void assign(std::initializer_list<value_type> init)
-		{
-			_container.assign(init);
-			this->_make_heap();
-		}
-		template <class InputIterator>
-		void assign(InputIterator begin, InputIterator end)
-		{
-			_container.assign(begin, end);
-			this->_make_heap();
-		}
+        heap& operator=(std::initializer_list<value_type> init)
+        {
+            _container = init;
+            this->_make_heap();
+        }
+    public:
+        void assign(size_type count, const_reference value)
+        {
+            _container.assign(count, value);
+            this->_make_heap();
+        }
+        void assign(std::initializer_list<value_type> init)
+        {
+            _container.assign(init);
+            this->_make_heap();
+        }
+        template <class InputIterator>
+        void assign(InputIterator begin, InputIterator end)
+        {
+            _container.assign(begin, end);
+            this->_make_heap();
+        }
 
-	public: //container와의 호환용
-		heap(const Container& vec) : _container(vec)
-		{
-			this->_make_heap();
-		}
-		heap(Container&& vec): _container(vec)
-		{
-			this->_make_heap();
-		}
-		operator const Container&() const
-		{
-			return this->_container;
-		}
+    public: //container와의 호환용
+        heap(const Container& vec) : _container(vec)
+        {
+            this->_make_heap();
+        }
+        heap(Container&& vec): _container(vec)
+        {
+            this->_make_heap();
+        }
+        operator const Container&() const
+        {
+            return this->_container;
+        }
 
-	public: //return sorted container
-		Container sorted() const
-		{
-			Container clone = this->_container;
-			std::sort_heap(clone.begin(), clone.end(), Compare());
-			return clone;
-		}
+    public: //return sorted container
+        Container sorted() const
+        {
+            Container clone = this->_container;
+            std::sort_heap(clone.begin(), clone.end(), Compare());
+            return clone;
+        }
 
-	public: //최대 힙을 기준으로, 최대값을 가져옵니다.
-		const_reference front() const
-		{
-			return _container.front();
-		}
+    public: //최대 힙을 기준으로, 최대값을 가져옵니다.
+        const_reference front() const
+        {
+            return _container.front();
+        }
 
-	public: //요소를 추가합니다.
-		void push(const_reference value)
-		{
-			_container.push_back(value);
-			this->push_heap();
-		}
-		void push(value_type&& value)
-		{
-			_container.push_back(std::move(value));
-			this->push_heap();
-		}
-		template <class ...Args>
-		void emplace(Args&&... args)
-		{
-			_container.emplace_back(std::forward<Args>(args)...);
-			this->push_heap();
-		}
+    public: //요소를 추가합니다.
+        void push(const_reference value)
+        {
+            _container.push_back(value);
+            this->push_heap();
+        }
+        void push(value_type&& value)
+        {
+            _container.push_back(std::move(value));
+            this->push_heap();
+        }
+        template <class ...Args>
+        void emplace(Args&&... args)
+        {
+            _container.emplace_back(std::forward<Args>(args)...);
+            this->push_heap();
+        }
 
-	public: //최대 힙을 기준으로, 최대값을 제거합니다.
-		void pop()
-		{
-			std::pop_heap(_container.begin(), _container.end(), Compare());
-			_container.pop_back();
-		}
+    public: //최대 힙을 기준으로, 최대값을 제거합니다.
+        void pop()
+        {
+            std::pop_heap(_container.begin(), _container.end(), Compare());
+            _container.pop_back();
+        }
 
-	public:
-		void clear() noexcept
-		{
-			return _container.clear();
-		}
-		void swap(Self& other) noexcept
-		{
-			this->_container.swap(other._container);
-		}
+    public:
+        void clear() noexcept
+        {
+            return _container.clear();
+        }
+        void swap(Self& other) noexcept
+        {
+            this->_container.swap(other._container);
+        }
 
-	public:
-		bool empty() const noexcept
-		{
-			return _container.empty();
-		}
-		size_type size() const noexcept
-		{
-			return _container.size();
-		}
-	};
+    public:
+        bool empty() const noexcept
+        {
+            return _container.empty();
+        }
+        size_type size() const noexcept
+        {
+            return _container.size();
+        }
+    };
 }
