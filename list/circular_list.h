9b64deb7bd8187442c54c00e1fe85cd4caa46ef2
sanggeon <sssang97@svcorps.dev>
2020-06-15 07:25:10 +0900

    remove memory leak

--- list/circular_list.h
+++ list/circular_list.h
@@ -15,35 +15,40 @@ namespace xstl
         struct node_type;
         using size_type = std::size_t;
         using difference_type = std::ptrdiff_t;
-        using reference = value_type & ;
-        using const_reference = const value_type&;
+        using reference = value_type &;
+        using const_reference = const value_type &;
         using pointer = value_type *;
-        using const_pointer = const value_type*;
+        using const_pointer = const value_type *;
         class iterator;
         class const_iterator;
         using reverse_iterator = std::reverse_iterator<iterator>;
         using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
     private:
-        node_type* _head = nullptr;
+        node_type *_head = nullptr;
         size_type _length = 0;
 
     public: //list node type
         struct node_type
         {
             using Self = node_type;
+
         public:
-            Self* prev;
+            Self *prev;
             value_type value;
-            Self* next;
+            Self *next;
+
         public:
             node_type() = delete;
             virtual ~node_type() = default;
-            node_type(Self* _prev, const_reference _value, Self* _next)
+            node_type(Self *_prev, const_reference _value, Self *_next)
                 : prev(_prev), value(_value), next(_next)
-            {}
-            node_type(Self* _prev, value_type&& _value, Self* _next)
+            {
+            }
+            node_type(Self *_prev, value_type &&_value, Self *_next)
                 : prev(_prev), value(std::move(_value)), next(_next)
-            {}
+            {
+            }
         };
 
     public:
@@ -61,7 +66,7 @@ namespace xstl
         }
         circular_list(std::initializer_list<value_type> init)
         {
-            for (auto& e : init)
+            for (auto &e : init)
                 this->push_back(e);
         }
         template <class InputIterator>
@@ -81,7 +86,7 @@ namespace xstl
         void assign(std::initializer_list<value_type> init)
         {
             this->clear();
-            for (auto& e : init)
+            for (auto &e : init)
                 this->push_back(e);
         }
         template <class InputIterator>
@@ -91,21 +96,22 @@ namespace xstl
             for (; begin != end; begin++)
                 this->push_back(*begin);
         }
-        
+
     public: // copy&move member
-        circular_list(const Self& other): circular_list(other.begin(), other.end())
-        {}
-        circular_list(Self&& other): _head(other._head), _length(other._length)
+        circular_list(const Self &other) : circular_list(other.begin(), other.end())
+        {
+        }
+        circular_list(Self &&other) : _head(other._head), _length(other._length)
         {
             other._head = nullptr;
             other._length = 0;
         }
-        Self& operator=(const Self& other)
+        Self &operator=(const Self &other)
         {
             this->assign(other.begin(), other.end());
             return *this;
         }
-        Self& operator=(Self&& other)
+        Self &operator=(Self &&other)
         {
             this->_head = other._head;
             this->_length = other._length;
@@ -155,7 +161,7 @@ namespace xstl
         }
 
         //Swap
-        void swap(Self& other) noexcept
+        void swap(Self &other) noexcept
         {
             auto temp_head = this->_head;
             auto temp_length = this->_length;
@@ -170,10 +176,10 @@ namespace xstl
     public: //front modifier
         void push_front(const_reference value)
         {
-            if(this->_head==nullptr)
+            if (this->_head == nullptr)
             {
                 this->_head = new node_type(nullptr, value, nullptr);
-                _head->prev = _head; 
+                _head->prev = _head;
                 _head->next = _head;
             }
             else
@@ -181,10 +187,10 @@ namespace xstl
                 this->_head = new node_type(this->_head->prev, value, this->_head);
                 _head->next->prev = _head;
             }
-            
+
             _length++;
         }
-        void push_front(value_type&& value)
+        void push_front(value_type &&value)
         {
             if (this->_head == nullptr)
             {
@@ -200,7 +206,7 @@ namespace xstl
 
             _length++;
         }
-        
+
         void pop_front()
         {
             assert(this->_head != nullptr);
@@ -210,19 +216,19 @@ namespace xstl
                 delete this->_head;
                 this->_head = nullptr;
             }
-            else 
+            else
             {
                 auto original_head = this->_head;
                 this->_head = this->_head->next;
                 this->_head->prev = original_head->prev;
                 delete original_head;
             }
-            
+
             this->_length--;
         }
-        
+
         template <class... Args>
-        void emplace_front(Args&&... args)
+        void emplace_front(Args &&... args)
         {
             if (this->_head == nullptr)
             {
@@ -256,7 +262,7 @@ namespace xstl
 
             _length++;
         }
-        void push_back(value_type&& value)
+        void push_back(value_type &&value)
         {
             if (this->_head == nullptr)
             {
@@ -282,19 +288,19 @@ namespace xstl
                 delete this->_head;
                 this->_head = nullptr;
             }
-            else 
+            else
             {
                 auto original_back = this->_head->prev;
                 this->_head->prev = original_back->prev;
                 original_back->prev->next = this->_head;
                 delete original_back;
             }
-            
+
             this->_length--;
         }
-        
+
         template <class... Args>
-        void emplace_back(Args&&... args)
+        void emplace_back(Args &&... args)
         {
             if (this->_head == nullptr)
             {
@@ -314,18 +320,18 @@ namespace xstl
     public:
         iterator insert(const_iterator pos, const_reference value)
         {
-            auto& before = const_cast<node_type*&>(pos.current);
-            auto& after = pos.current->next;
+            auto &before = const_cast<node_type *&>(pos.current);
+            auto &after = pos.current->next;
             before->next = new node_type(before, value, after);
             after->prev = before->next;
 
             this->_length++;
             return iterator(before->next);
         }
-        iterator insert(const_iterator pos, value_type&& value)
+        iterator insert(const_iterator pos, value_type &&value)
         {
-            auto& before = const_cast<node_type*&>(pos.current);
-            auto& after = pos.current->next;
+            auto &before = const_cast<node_type *&>(pos.current);
+            auto &after = pos.current->next;
             before->next = new node_type(before, std::move(value), after);
             after->prev = before->next;
 
@@ -350,7 +356,7 @@ namespace xstl
         {
             auto position = pos;
 
-            for (;begin!=end; begin++)
+            for (; begin != end; begin++)
             {
                 position = this->insert(position, *begin);
             }
@@ -361,7 +367,7 @@ namespace xstl
         {
             auto position = pos;
 
-            for (auto& e:init)
+            for (auto &e : init)
             {
                 position = this->insert(position, e);
             }
@@ -370,11 +376,11 @@ namespace xstl
         }
 
         template <class... Args>
-        iterator emplace(const_iterator pos, Args&&... args)
+        iterator emplace(const_iterator pos, Args &&... args)
         {
-            auto& before = const_cast<node_type*&>(pos.current);
-            auto& after = pos.current->next;
-            before->next = new node_type(before, value_type(std::forward<Args>(args)...) , after);
+            auto &before = const_cast<node_type *&>(pos.current);
+            auto &after = pos.current->next;
+            before->next = new node_type(before, value_type(std::forward<Args>(args)...), after);
             after->prev = before->next;
 
             this->_length++;
@@ -390,14 +396,15 @@ namespace xstl
             if (this->_length == 1)
             {
                 assert(this->_head == pos.current);
+                delete pos.current;
                 this->_head = nullptr;
 
                 return iterator(nullptr);
             }
             else
             {
-                auto& before = pos.current->prev;
-                auto& after = pos.current->next;
+                auto &before = pos.current->prev;
+                auto &after = pos.current->next;
 
                 if (this->_head == pos.current)
                     this->_head = after;
@@ -412,8 +419,8 @@ namespace xstl
         }
         iterator erase(const_iterator begin, const_iterator end)
         {
-            auto& before = const_cast<node_type*&>(begin.current->prev);
-            auto& after = const_cast<node_type*&>(end.current);
+            auto &before = const_cast<node_type *&>(begin.current->prev);
+            auto &after = const_cast<node_type *&>(end.current);
 
             before->next = after;
             after->prev = before;
@@ -434,8 +441,8 @@ namespace xstl
 
         void resize(size_type count, const_reference new_value = value_type())
         {
-            if(count > this->_length)
-            { 
+            if (count > this->_length)
+            {
                 size_type difference = count - _length;
                 for (int i = 0; i < difference; i++)
                     this->push_back(new_value);
@@ -508,32 +515,39 @@ namespace xstl
         class iterator
         {
         private:
-            node_type* current;
+            node_type *current;
+
         public:
             using Self = iterator;
             friend const_iterator;
             friend circular_list;
+
         public:
             using value_type = value_type;
             using pointer = pointer;
             using reference = reference;
             using difference_type = std::ptrdiff_t;
             using iterator_category = std::bidirectional_iterator_tag;
+
         public:
             iterator() = delete;
             virtual ~iterator() = default;
+
         public: //move & copy member
-            iterator(const Self&) = default;
-            iterator(Self&&) = default;
-            Self& operator=(const Self&) = default;
-            Self& operator=(Self&&) = default;
+            iterator(const Self &) = default;
+            iterator(Self &&) = default;
+            Self &operator=(const Self &) = default;
+            Self &operator=(Self &&) = default;
+
         public:
-            iterator(node_type* p): current(p) 
-            {}
-        public: //move operator
-            Self& operator++()
+            iterator(node_type *p) : current(p)
             {
-                assert(current!=nullptr);
+            }
+
+        public: //move operator
+            Self &operator++()
+            {
+                assert(current != nullptr);
                 current = current->next;
                 return *this;
             }
@@ -543,7 +557,7 @@ namespace xstl
                 current = current->next;
                 return *this;
             }
-            Self& operator--()
+            Self &operator--()
             {
                 assert(current != nullptr);
                 current = current->prev;
@@ -555,6 +569,7 @@ namespace xstl
                 current = current->prev;
                 return *this;
             }
+
         public: //access operator
             reference operator*()
             {
@@ -576,12 +591,13 @@ namespace xstl
                 assert(current != nullptr);
                 return &current->value;
             }
+
         public: //comparer
-            bool operator==(const Self& other) const
+            bool operator==(const Self &other) const
             {
                 return this->current == other.current;
             }
-            bool operator!=(const Self& other) const
+            bool operator!=(const Self &other) const
             {
                 return this->current != other.current;
             }
@@ -590,31 +606,39 @@ namespace xstl
         class const_iterator
         {
         private:
-            const node_type* current;
+            const node_type *current;
+
         public:
             using Self = const_iterator;
             friend circular_list;
+
         public:
             using value_type = value_type;
             using pointer = pointer;
             using reference = reference;
             using difference_type = std::ptrdiff_t;
             using iterator_category = std::bidirectional_iterator_tag;
+
         public:
             const_iterator() = delete;
             ~const_iterator() = default;
+
         public: //move & copy member
-            const_iterator(const Self&) = default;
-            const_iterator(Self&&) = default;
-            Self& operator=(const Self&) = default;
-            Self& operator=(Self&&) = default;
+            const_iterator(const Self &) = default;
+            const_iterator(Self &&) = default;
+            Self &operator=(const Self &) = default;
+            Self &operator=(Self &&) = default;
+
         public:
-            const_iterator(node_type* p) : current(p)
-            {}
-            const_iterator(const iterator& mutable_iterator): current(mutable_iterator.current)
-            {}
+            const_iterator(node_type *p) : current(p)
+            {
+            }
+            const_iterator(const iterator &mutable_iterator) : current(mutable_iterator.current)
+            {
+            }
+
         public: //move operator
-            Self& operator++()
+            Self &operator++()
             {
                 assert(current != nullptr);
                 current = current->next;
@@ -626,7 +650,7 @@ namespace xstl
                 current = current->next;
                 return *this;
             }
-            Self& operator--()
+            Self &operator--()
             {
                 assert(current != nullptr);
                 current = current->prev;
@@ -638,6 +662,7 @@ namespace xstl
                 current = current->prev;
                 return *this;
             }
+
         public: //access operator
             const_reference operator*() const
             {
@@ -649,15 +674,16 @@ namespace xstl
                 assert(current != nullptr);
                 return &current->value;
             }
+
         public: //comparer
-            bool operator==(const Self& other) const
+            bool operator==(const Self &other) const
             {
                 return this->current == other.current;
             }
-            bool operator!=(const Self& other) const
+            bool operator!=(const Self &other) const
             {
                 return this->current != other.current;
             }
         };
     };
-}
+} // namespace xstl
